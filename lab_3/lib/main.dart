fd2c15b21b531fbe85f85eb80c3b3098f2d5c88a
damyzo <ilievskidamjan4@gmail.com>
2021-12-19 01:26:06 +0100

    Lab_3 created

--- lab_3/lib/main.dart
+++ lab_3/lib/main.dart
@@ -31,15 +31,28 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  List<String> elements = [
+    'Prv',
+    'Vtor',
+    'Tret',
+    'Cetvrt',
+    'Pet',
+    'Sest',
+    'Sedum',
+    'Osum',
+    'Devet',
+    'Deset',
+    'Edinaeset',
+    'Dvanaeset',
+    'Trinaeset',
+    'Cetirinaeset',
+    'Petnaeset',
+    'Sesnaeset',
+    'Sedumnaeset',
+    'Osumnaeset',
+    'Devetnaeset',
+    'Dvaeset',
+  ];
 
   final String title;
 
@@ -49,6 +62,8 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  
+  
 
   void _incrementCounter() {
     setState(() {
@@ -63,6 +78,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    
     // This method is rerun every time setState is called, for instance as done
     // by the _incrementCounter method above.
     //
@@ -71,45 +87,29 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(title),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          const TextField(),
+          const TextField(),
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: elements.length,
+              itemBuilder: (contx, index) {
+                return ExamDate(name: widget.elements[index], date: elements[index]);
+              },
+            ),),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+
     );
   }
 }
